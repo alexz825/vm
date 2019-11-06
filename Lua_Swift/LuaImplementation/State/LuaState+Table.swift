4a92e2a5f4f61b184e420a9d6efe486735eff036
zhuruliang <chenmu@xiaohongshu.com>
2019-11-06 19:53:20 +0800

    add table

---
+++ Lua_Swift/LuaImplementation/State/LuaState+Table.swift
@@ -0,0 +1,13 @@
+//
+//  LuaState+Table.swift
+//  LuaImplementation
+//
+//  Created by chenmu on 2019/11/6.
+//  Copyright © 2019 AlexZHU. All rights reserved.
+//
+
+import Cocoa
+
+class LuaState_Table: NSObject {
+
+}
